0cad1df39b23032ce19718a705549604aa2aed11
luka <luka.janjgava.1@btu.edu.ge>
2022-08-21 20:43:16 +0400

    network layer added

--- app/src/main/java/com/lukabaia/yummy/ui/MainActivity.kt
+++ app/src/main/java/com/lukabaia/yummy/ui/MainActivity.kt
@@ -2,7 +2,11 @@ package com.lukabaia.yummy.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.lifecycle.lifecycleScope
 import com.lukabaia.yummy.R
+import com.lukabaia.yummy.network.RetrofitInstance
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
