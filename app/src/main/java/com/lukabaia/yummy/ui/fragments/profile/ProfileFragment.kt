358dfcc742f48354c45172541f9df6b9f20bb216
Baia Asanidze <baia.asanidze.1@btu.edu.ge>
2022-08-22 17:51:39 +0400

    add profileInfo from database

--- app/src/main/java/com/lukabaia/yummy/ui/fragments/profile/ProfileFragment.kt
+++ app/src/main/java/com/lukabaia/yummy/ui/fragments/profile/ProfileFragment.kt
@@ -1,7 +1,13 @@
 package com.lukabaia.yummy.ui.fragments.profile
 
 import androidx.fragment.app.viewModels
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.lukabaia.yummy.databinding.FragmentProfileBinding
+import com.lukabaia.yummy.model.UserInfo
 import com.lukabaia.yummy.ui.fragments.base.BaseFragment
 import com.lukabaia.yummy.viewModels.NoInternetViewModel
 
@@ -9,16 +15,30 @@ class ProfileFragment : BaseFragment<FragmentProfileBinding>(FragmentProfileBind
 
     private val viewModel: NoInternetViewModel by viewModels()
 
+    private val auth = FirebaseAuth.getInstance()
+    private val database = FirebaseDatabase.getInstance().getReference("userInfo")
+
     override fun listeners() {
-        TODO("Not yet implemented")
     }
 
     override fun init() {
-        TODO("Not yet implemented")
+
+        //database
+
+        binding.tvEmail.text = FirebaseAuth.getInstance().currentUser?.email
+
+        database.child(auth.currentUser?.uid!!).addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val userInfo = snapshot.getValue(UserInfo::class.java) ?: return
+
+                binding.tvUsername.text = userInfo.username
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+            }
+        })
     }
 
     override fun observers() {
-        TODO("Not yet implemented")
     }
-
 }
