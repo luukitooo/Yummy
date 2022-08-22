8cc50b7414358ff8d0c67da121461493899c0969
luka <luka.janjgava.1@btu.edu.ge>
2022-08-22 16:38:24 +0400

    repository layer added & adapters used

--- app/src/main/java/com/lukabaia/yummy/ui/fragments/home/HomeFragment.kt
+++ app/src/main/java/com/lukabaia/yummy/ui/fragments/home/HomeFragment.kt
@@ -1,21 +1,57 @@
 package com.lukabaia.yummy.ui.fragments.home
 
-import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import com.lukabaia.yummy.adapter.CategoryAdapter
+import com.lukabaia.yummy.adapter.RecipeAdapter
 import com.lukabaia.yummy.databinding.FragmentHomeBinding
 import com.lukabaia.yummy.ui.fragments.base.BaseFragment
+import com.lukabaia.yummy.utils.RecipeTypes
 import com.lukabaia.yummy.viewModels.HomeViewModel
+import kotlinx.coroutines.launch
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::inflate) {
 
-    private val viewModel: HomeViewModel by viewModels()
+    private val viewModel by viewModel<HomeViewModel>()
 
-    override fun listeners() {
-    }
+    private val categoryAdapter by lazy { CategoryAdapter() }
+    private val recipeAdapter by lazy { RecipeAdapter() }
 
     override fun init() {
+
+        buildRecyclers()
+
+        getData(RecipeTypes.values()[0].value)
+
+    }
+
+    override fun listeners() {
+        categoryAdapter.onItemClickListener = {
+            getData(it.value)
+        }
     }
 
     override fun observers() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewModel.recipesFlow.collect {
+                recipeAdapter.submitList(it?.results ?: emptyList())
+            }
+        }
+    }
+
+    private fun buildRecyclers() = with(binding) {
+        rvCategories.adapter = categoryAdapter
+        rvRecipes.adapter = recipeAdapter
+        categoryAdapter.submitList(RecipeTypes.values().toList())
+    }
+
+    private fun getData(type: String) {
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewModel.getSearchResult(
+                type = type,
+                number = 5
+            )
+        }
     }
 
 }
--- app/src/main/java/com/lukabaia/yummy/viewModels/HomeViewModel.kt
+++ app/src/main/java/com/lukabaia/yummy/viewModels/HomeViewModel.kt
@@ -1,7 +1,25 @@
 package com.lukabaia.yummy.viewModels
 
 import androidx.lifecycle.ViewModel
+import com.lukabaia.yummy.model.SearchedRecipesInfo
+import com.lukabaia.yummy.repository.HomeRepository
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
 
-class HomeViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class HomeViewModel(
+    private val homeRepository: HomeRepository
+) : ViewModel() {
+
+    private val _recipesFlow = MutableSharedFlow<SearchedRecipesInfo?>()
+    val recipesFlow = _recipesFlow.asSharedFlow()
+
+    suspend fun getSearchResult(type: String, number: Int) {
+        homeRepository.getSearchResult(
+            type = type,
+            number = number
+        ).also {
+            _recipesFlow.emit(it)
+        }
+    }
+
 }
