d870db06c6b56dbf0e589d83eaa151b8ac120a19
Baia Asanidze <baia.asanidze.1@btu.edu.ge>
2022-08-23 23:42:29 +0400

    edit profile

--- app/src/main/java/com/lukabaia/yummy/ui/fragments/profile/ProfileFragment.kt
+++ app/src/main/java/com/lukabaia/yummy/ui/fragments/profile/ProfileFragment.kt
@@ -1,39 +1,102 @@
 package com.lukabaia.yummy.ui.fragments.profile
 
+import android.app.Activity.RESULT_OK
+import android.content.Intent
+import android.net.Uri
 import android.util.Log.d
 import android.widget.Toast
+import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ValueEventListener
-import com.lukabaia.yummy.R
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 import com.lukabaia.yummy.databinding.FragmentProfileBinding
-import com.lukabaia.yummy.model.UserInfo
 import com.lukabaia.yummy.ui.fragments.base.BaseFragment
 import com.lukabaia.yummy.utils.ResultOf
-import com.lukabaia.yummy.viewModels.NoInternetViewModel
 import com.lukabaia.yummy.viewModels.ProfileViewModel
 import kotlinx.coroutines.launch
+
 
 class ProfileFragment : BaseFragment<FragmentProfileBinding>(FragmentProfileBinding::inflate) {
 
     private val viewModel: ProfileViewModel by viewModels()
+    private var imageUri: Uri? = null
+    private var firebaseStore : FirebaseStorage? = null
+    private var storageReference: StorageReference? = null
+    private var auth : FirebaseAuth? = null
+    private val PICK_IMAGE_REQUEST = 71
 
     override fun listeners() {
+
+        binding.tvChange.setOnClickListener {
+
+            binding.btnSelectImage.isVisible = true
+            binding.btnUploadImage.isVisible = true
+        }
+
+        binding.btnSelectImage.setOnClickListener {
+
+            selectImage()
+
+        }
+        binding.btnUploadImage.setOnClickListener {
+
+            uploadImage()
+
+
+        }
+
+    }
+
+    private fun selectImage() {
+        val intent = Intent()
+        intent.type = "image/"
+        intent.action = Intent.ACTION_GET_CONTENT
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST)
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK) {
+
+            if(data == null || data.data == null){
+                return
+            }
+
+            imageUri = data.data
+            binding.imgProfileImage.setImageURI(imageUri)
+        }
+    }
+
+    private fun uploadImage() {
+
+        if(imageUri != null){
+            val reference = storageReference?.child(auth!!.currentUser!!.uid)
+            val uploadTask = reference?.putFile(imageUri!!)
+            Toast.makeText(context, "Successfully uploaded!", Toast.LENGTH_SHORT).show()
+            binding.btnSelectImage.isVisible = false
+            binding.btnUploadImage.isVisible = false
+        } else{
+            Toast.makeText(context, "Please Upload an Image", Toast.LENGTH_SHORT).show()
+
+        }
+
     }
 
     override fun init() {
+        auth = FirebaseAuth.getInstance()
+        firebaseStore = FirebaseStorage.getInstance()
+        storageReference = FirebaseStorage.getInstance().reference.child("Profile Img")
         // database
         showData()
         observers()
     }
 
-    private fun showData(){
+    private fun showData() {
         val userName = binding.tvUsername
         val email = binding.tvEmail
         viewModel.showRealtimeData(userName, email)
@@ -56,6 +119,7 @@ class ProfileFragment : BaseFragment<FragmentProfileBinding>(FragmentProfileBind
             }
         }
     }
+
 
 //    private val auth = FirebaseAuth.getInstance()
 //    private val database = FirebaseDatabase.getInstance().getReference("userInfo")
